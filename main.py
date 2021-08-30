acd8d087946d311003f8d01ddbc6c822471a8f53
Behnam Bahrami <behnambahrami31@gmail.com>
2021-08-30 21:22:12 +0430

    add countdown mechanism

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +11,22 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(WORK_MIN * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count >= 0:
+        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -24,7 +37,7 @@ window.config(bg=YELLOW, padx=100, pady=50)
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, 'normal'))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", bg=PINK, highlightthickness=0)
+start_button = Button(text="Start", bg=PINK, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", bg=PINK, highlightthickness=0)
@@ -36,13 +49,7 @@ check_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 111, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, 'bold'))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, 'bold'))
 canvas.grid(column=1, row=1)
-
-
-
-
-
-
 
 window.mainloop()
