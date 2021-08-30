e550b1fa2acdd329525e6bd2222210cf27e21863
Behnam Bahrami <behnambahrami31@gmail.com>
2021-08-30 20:37:13 +0430

    add start reset button and checkmark label

--- main.py
+++ main.py
@@ -19,13 +19,25 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("pomodoro")
-window.config(bg=YELLOW)
+window.config(bg=YELLOW, padx=100, pady=50)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, 'normal'))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", bg=PINK, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", bg=PINK, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_label = Label(text="✔", bg=YELLOW)
+check_label.grid(column=1, row=3)
 
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(101, 111, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 20, 'bold'))
-canvas.pack(padx=100, pady=100)
+canvas.create_image(100, 111, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, 'bold'))
+canvas.grid(column=1, row=1)
 
 
 
