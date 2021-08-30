7b22e5e2a024d333f9b440b0b915ffce25c375c7
Behnam Bahrami <behnambahrami31@gmail.com>
2021-08-30 22:54:33 +0430

    adding checkmark and resettting the application

--- main.py
+++ main.py
@@ -6,28 +6,56 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+LIGHT = "#FFFFFF"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    global reps
+    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    countdown(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+    checkmark_symbol = "✔"
+    check_label.config(text=checkmark_symbol * int(reps / 2))
+    if reps == 8:
+        timer_label.config(text="REST", fg=RED)
+        countdown(long_break)
+    elif reps % 2 == 1:
+        timer_label.config(text="WORK")
+        countdown(work_sec)
+    else:
+        timer_label.config(text="BREAK", fg=PINK)
+        countdown(short_break)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
-    count_min = math.floor(count/60)
+    count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
         count_sec = f"0{count_sec}"
-    if count >= 0:
-        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-        window.after(1000, countdown, count - 1)
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -39,13 +67,13 @@ window.config(bg=YELLOW, padx=100, pady=50)
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, 'normal'))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", bg=PINK, highlightthickness=0, command=start_timer)
+start_button = Button(text="Start", bg=LIGHT, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", bg=PINK, highlightthickness=0)
+reset_button = Button(text="Reset", bg=LIGHT, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_label = Label(text="✔", bg=YELLOW)
+check_label = Label(bg=YELLOW)
 check_label.grid(column=1, row=3)
 
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
